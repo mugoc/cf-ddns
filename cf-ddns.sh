6e48562dc3c6612ca9452f3987ea87e7080aa149
root <root@debian-BULLSEYE-live-builder-AMD64>
2021-11-02 22:21:47 +0800

    no ip change

--- cf-ddns.sh
+++ cf-ddns.sh
@@ -4,6 +4,7 @@ Zone_ID=""			# 概况中的Zone_ID
 Email=""			# CloudFlare注册邮箱
 Key=""				# Global Key
 Records_NAME=""		# 需要更新IP的域名
+ip_file="ipv4.txt" 
 
 # 替换下方的zone_ID,X-Auth-Key,X-Auth-Email在命令行复制，并保存返回值，查找需要修改的域名，如muge.example.com
 # 返回值中找到对应域名的IP，替换到下发的Records_ID
@@ -20,10 +21,19 @@ Records_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/${Zone_I
 IPV4=$(curl -s ip.sb)
 echo "当前IP：${IPV4}"
 
+if [ -f $ip_file ]; then
+    old_ip=$(cat $ip_file)
+        if [ ${IPV4} == ${old_ip} ]; then
+	        echo "IP has not changed."
+	        exit 0
+	fi
+fi
+
 updata=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/${Zone_ID}/dns_records/${Records_ID}" \
      -H "X-Auth-Email: ${Email}" \
      -H "X-Auth-Key: ${Key}" \
      -H "Content-Type: application/json" \
      --data '{"type":"A","name":"'${Records_NAME}'","content":"'${IPV4}'","ttl":60,"proxied":false}')
 
+echo "${IPV4}">ipv4.txt
 echo "IP更新完成：${Records_NAME}=>${IPV4}"
